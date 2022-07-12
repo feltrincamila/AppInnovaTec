4dd599795d313d2150cba6a6ebcf8f5a8a69d4e4
vanysai <92987278+diego12276ur6y6@users.noreply.github.com>
2022-07-12 11:18:49 -0300

    First Commit

---
+++ app/src/main/java/com/example/innovatec3b/SegundaTelaVagas.java
@@ -0,0 +1,14 @@
+package com.example.innovatec3b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundaTelaVagas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_tela_vagas);
+    }
+}
